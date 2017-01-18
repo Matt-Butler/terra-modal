d9e9408ac2fcbf172e2b3bb08ade92bcfbb24616
Matt Butler <mattwbutler@gmail.com>
2017-01-18 16:16:44 -0600

    storyshots working

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,4 +1,5 @@
 const autoprefixer = require('autoprefixer');
+const path = require('path');
 
 module.exports = {
   module: {
@@ -8,6 +9,9 @@ module.exports = {
         loader: 'style!css!postcss!sass',
       },
     ],
+  },
+  sassLoader: {
+    data: `@import "${path.resolve('node_modules/terra-legacy-theme/src/terra-legacy-theme.scss')}";`,
   },
   postcss: [
     autoprefixer({
